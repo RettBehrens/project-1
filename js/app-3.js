2854986ed6191e462fe091d8b051d18ca13ee114
RettBehrens <everettbehrens@gmail.com>
2017-07-15 17:01:36 -0600

    toggles class on click and resets gameboard on reset

--- js/app-3.js
+++ js/app-3.js
@@ -16,7 +16,8 @@ function makeReady() {
 	};
 	console.log("reset ready!");
 	document.getElementById("reset").onclick = function() {
-		wipeGame();
+		wipeGameTimer();
+		wipeGameBoard();
 	};
 	makeGameboard();
 }
@@ -43,14 +44,33 @@ function makeGameboard() {
 }
 
 //wipeGame() resets gameboard to pre-play state and clears player scores
-function wipeGame() {
+function wipeGameTimer() {
 	console.log("wipe game works");
-	var gameboardGrid = document.getElementById("ggId");
-	ggId.parentNode.removeChild(ggId);
 	document.getElementById("timer").innerHTML = "TIMER";
 	timeLeft = 10;
 	//document.getElementById("p-1-score").innerHTML = "0";
 	//document.getElementById("p-2-score").innerHTML = "0";
+}
+
+function wipeGameBoard() {
+	document.getElementById("mH1").classList.remove("mole");
+	document.getElementById("mH2").classList.remove("mole");
+	document.getElementById("mH3").classList.remove("mole");
+	document.getElementById("mH4").classList.remove("mole");
+	document.getElementById("mH5").classList.remove("mole");
+	document.getElementById("mH6").classList.remove("mole");
+	document.getElementById("mH7").classList.remove("mole");
+	document.getElementById("mH8").classList.remove("mole");
+	document.getElementById("mH9").classList.remove("mole");
+	document.getElementById("mH1").removeEventListener("click", giveMePoints, true);
+	document.getElementById("mH2").removeEventListener("click", giveMePoints, true);
+	document.getElementById("mH3").removeEventListener("click", giveMePoints, true);
+	document.getElementById("mH4").removeEventListener("click", giveMePoints, true);
+	document.getElementById("mH5").removeEventListener("click", giveMePoints, true);
+	document.getElementById("mH6").removeEventListener("click", giveMePoints, true);
+	document.getElementById("mH7").removeEventListener("click", giveMePoints, true);
+	document.getElementById("mH8").removeEventListener("click", giveMePoints, true);
+	document.getElementById("mH9").removeEventListener("click", giveMePoints, true);
 }
 
 function ready() {
@@ -94,10 +114,10 @@ function startGameTimer () {
 
 var points = 0;
 function giveMePoints() {
-	console.log("points plus 1");
 	//needs an if-else statement dictating who gets points
 	if (timeLeft > 0) {
 	document.getElementById("p-1-score").innerHTML = points += 1;
+	console.log("points plus 1");
 	} else {
 		document.getElementById("p-1-score").innerHTML = points;
 	}
@@ -108,7 +128,7 @@ var myIntervalVariable;
 
 function mySetInterval() {
 	document.getElementById("timer").innerHTML = "GO!";
-	myIntervalVariable = setInterval(activateMoles, 2000);
+	myIntervalVariable = setInterval(activateMoles, 500);
 }
 
 function myClearInterval() {
@@ -129,8 +149,26 @@ function activateMoles() {
 	//add and remove click event listener to "mole" class
 	if (document.getElementById("mH"+ whichMoleHole).className === "mh mole") {
 		document.getElementById("mH"+ whichMoleHole).addEventListener("click", giveMePoints, true);
+		document.getElementById("mH"+ whichMoleHole).addEventListener("click", whacked, true);
 	 } else {
 		document.getElementById("mH"+ whichMoleHole).removeEventListener("click", giveMePoints, true);
+		document.getElementById("mH"+ whichMoleHole).removeEventListener("click", whacked, true);
 		console.log("no clicky no more");	
 	}
 }
+
+function whacked() {
+	if (timeLeft > 0) {
+		this.classList.remove("mole");
+		this.classList.add("whacked");
+		setTimeout(resetWhacked, 500);
+	}
+
+	function resetWhacked() {
+		this.classList.remove("whacked");
+	}
+
+}
+
+
+
