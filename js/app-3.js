c09cbbf881057c91c965d3914423b8d46cd6f070
RettBehrens <everettbehrens@gmail.com>
2017-07-16 14:05:25 -0600

    adds more css for arcade-ish look

--- js/app-3.js
+++ js/app-3.js
@@ -8,11 +8,13 @@ window.onload = function() {
 };
 
 //add click functions to "play" and "reset" buttons
-//when clicked, buttons call functions startGame() and wipeGame()
+//when clicked, buttons call functions ready() and wipeGameBoard() and wipeGameTimer
 function makeReady() {
 	console.log("play ready!");
 	document.getElementById("play").onclick = function() {
+		//"play" button disabled while game is in play
 		document.getElementById("play").disabled = true;
+		//delays the function that re-enables play button until game timer = 0
 		setTimeout(enablePlay, 14000);
 		ready();
 	};
@@ -24,10 +26,12 @@ function makeReady() {
 	makeGameboard();
 }
 
+//re-enables "play" button 
 function enablePlay() {
 	document.getElementById("play").disabled = false;
 }
 
+//makes gameboard and all child divs - mole holes where moles could pop up
 function makeGameboard() {
 	var gameboardGrid = document.createElement("div");
 	gameboardGrid.id = "ggId";
@@ -49,38 +53,42 @@ function makeGameboard() {
 	}
 }
 
-//wipeGame() resets gameboard to pre-play state and clears player scores
+//wipeGameTimer resets timer = 10
 function wipeGameTimer() {
 	console.log("wipe game works");
 	document.getElementById("timer").innerHTML = "TIMER";
 	timeLeft = 10;
-	//document.getElementById("p-1-score").innerHTML = "0";
-	//document.getElementById("p-2-score").innerHTML = "0";
 }
 
+//wipeGameBoard() resets game to pre-play state and clears player scores
 function wipeGameBoard() {
 	document.getElementById("p-1-score").innerHTML = 0;
 	document.getElementById("p-2-score").innerHTML = 0;
+	document.getElementById("declaration").innerHTML = "Player 1 Turn";
 }
 
+//timer reads "ready..." and calls countdownThree
 function ready() {
 	setTimeout(countdownThree, 1000);
 	document.getElementById("timer").innerHTML = "READY..";
 	console.log("ready.....");
 }
 
+//timer reads "three..." and calls countdownTwo
 function countdownThree() {
 	setTimeout(countdownTwo, 1000);
 	document.getElementById("timer").innerHTML = "THREE..";
 	console.log("Three.....");
 }
 
+//timer reads "two..." and calls countdownOne
 function countdownTwo() {
 	setTimeout(countdownOne, 1000);
 	document.getElementById("timer").innerHTML = "TWO..";
 	console.log("Two.....");
 }
 
+//timer reads "one..." and calls setInterval and gameTimer
 function countdownOne() {
 	setTimeout(mySetInterval, 1000);
 	document.getElementById("timer").innerHTML = "ONE..";
@@ -96,7 +104,6 @@ function startGameTimer () {
 	var gameTimer = setInterval(function(){
 		document.getElementById("timer").innerHTML = timeLeft -= 1;
   		if(timeLeft === 0) {
-  			//clearInterval(activateMole);
 	    	clearInterval(gameTimer);
 	    	document.getElementById("mH1").classList.remove("mole");
 			document.getElementById("mH2").classList.remove("mole");
@@ -125,13 +132,13 @@ function startGameTimer () {
 			document.getElementById("mH7").removeEventListener("click", whacked, true);
 			document.getElementById("mH8").removeEventListener("click", whacked, true);
 			document.getElementById("mH9").removeEventListener("click", whacked, true);
+			//this if-else statement controls which player is up and where points are allocated
 			if (player === player1) {
 				player = player2;
 				points = 0;
 				document.getElementById("declaration").innerHTML = "Player 2 Turn";
 			} else if (player === player2) {
-				//player = player1;
-				//points = 0;
+				//if player 1 and 2 have gone, announce winner
 				announceWinner();
 			}
 	    }
@@ -141,7 +148,7 @@ function startGameTimer () {
 var points = 0;
 var player = player1;
 function giveMePoints() {
-	//needs an if-else statement dictating who gets points
+	//if player 1 is playing, they get points. if player 2 is playing, they get points.
 	if (timeLeft > 0 && player === player1) {
 	document.getElementById("p-1-score").innerHTML = points += 1;
 	console.log("p1 points plus 1");
@@ -153,15 +160,18 @@ function giveMePoints() {
 
 var myIntervalVariable;
 
+//timer reads "go!" and moles activateMoles is called every half second
 function mySetInterval() {
 	document.getElementById("timer").innerHTML = "GO!";
 	myIntervalVariable = setInterval(activateMoles, 500);
 }
 
+//stops moles being generated
 function myClearInterval() {
 	clearInterval(myIntervalVariable);
 }
 
+//calls moles at random, every half second due to setInterval, until clearInterval is called
 function activateMoles() {
 	//math.random function used for calling individual cells
 	function whichMoleHole(min, max) {
@@ -184,6 +194,7 @@ function activateMoles() {
 	}
 }
 
+//provide user feedback visually indicating moles were whacked and calls function to return to standby
 function whacked() {
 	if (timeLeft > 0) {
 		this.classList.remove("mole");
@@ -193,10 +204,12 @@ function whacked() {
 	}
 }
 
+//after displaying whacked, this sets div back to empty mole hole or standby
 function resetWhacked() {
 	document.getElementsByClassName("whacked")[0].classList.remove("whacked");
 }
 
+//assuming both players have played, this function compares player scores and declares winner or tie
 function announceWinner() {
 	if (document.getElementById("p-1-score").innerHTML > document.getElementById("p-2-score").innerHTML) {
 		document.getElementById("declaration").innerHTML = "Player 1 Wins!";
